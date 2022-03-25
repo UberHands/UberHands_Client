923ed88b4e5bb9bd8bc622b788f62686fad49b08
Sujal Modanwal <sumo12082000@gmail.com>
2022-03-25 20:56:54 +0530

    Helper page updated

--- src/components/dashboard/helperDashboard.js
+++ src/components/dashboard/helperDashboard.js
@@ -25,7 +25,169 @@ function HelperDashboard() {
           <div className = "heading">
             <h1>Available Jobs Near You</h1>
           </div>
-          <div className = "card">
+          <div className = "profiles">
+            <div className = "card">
+              <div className = "lcolumn">
+                  <img src = "img/dog.jpeg"></img>
+              </div>
+              <div className = "rcolumn">
+                <h1>Dog Sitting</h1>
+                  <div className = "jobdetails">
+                    <div className = "rowdetails">
+                      <div className="col1">
+                        <i className='far fa-calendar-alt'></i><p>20 minutes</p>
+                      </div>
+                      <div className="col2">
+                        <i className='fas fa-clock'></i><p>30 minutes</p>
+                      </div>
+                    </div>
+                    <div className = "rowdetails">
+                      <div className = "col1">
+                        <i className='fa fa-rupee'></i><p>80</p>
+                      </div>
+                      <div className = "col2">
+                        <i className='fas fa-map-marker'></i><p>3 km</p>
+                      </div>
+                    </div>
+                  </div>
+                    <button>Pick This Job!</button>
+              </div>
+            </div>
+            <div className = "card">
+              <div className = "lcolumn">
+                  <img src = "img/Gardening.jpg"></img>
+              </div>
+              <div className = "rcolumn">
+                <h1>Gardening</h1>
+                  <div className = "jobdetails">
+                    <div className = "rowdetails">
+                      <div className="col1">
+                        <i className='far fa-calendar-alt'></i><p> Tomm 2pm</p>
+                      </div>
+                      <div className="col2">
+                        <i className='fas fa-clock'></i><p>60 minutes</p>
+                      </div>
+                    </div>
+                    <div className = "rowdetails">
+                      <div className = "col1">
+                        <i className='fa fa-rupee'></i><p>150</p>
+                      </div>
+                      <div className = "col2">
+                        <i className='fas fa-map-marker'></i><p>1 km</p>
+                      </div>
+                    </div>
+                  </div>
+                    <button>Pick This Job!</button>
+              </div>
+            </div>
+            <div className = "card">
+              <div className = "lcolumn">
+                  <img src = "img/grocery.jpg"></img>
+              </div>
+              <div className = "rcolumn">
+                <h1>Grocery Shopping</h1>
+                  <div className = "jobdetails">
+                    <div className = "rowdetails">
+                      <div className="col1">
+                        <i className='far fa-calendar-alt'></i><p>20 minutes</p>
+                      </div>
+                      <div className="col2">
+                        <i className='fas fa-clock'></i><p>60 minutes</p>
+                      </div>
+                    </div>
+                    <div className = "rowdetails">
+                      <div className = "col1">
+                        <i className='fa fa-rupee'></i><p>120</p>
+                      </div>
+                      <div className = "col2">
+                        <i className='fas fa-map-marker'></i><p>3 km</p>
+                      </div>
+                    </div>
+                  </div>
+                    <button>Pick This Job!</button>
+              </div>
+            </div>
+            <div className = "card">
+              <div className = "lcolumn">
+                  <img src = "img/driving.jpg"></img>
+              </div>
+              <div className = "rcolumn">
+                <h1>Driving</h1>
+                  <div className = "jobdetails">
+                    <div className = "rowdetails">
+                      <div className="col1">
+                        <i className='far fa-calendar-alt'></i><p>Thurs 4pm</p>
+                      </div>
+                      <div className="col2">
+                        <i className='fas fa-clock'></i><p>2 Days</p>
+                      </div>
+                    </div>
+                    <div className = "rowdetails">
+                      <div className = "col1">
+                        <i className='fa fa-rupee'></i><p>1500</p>
+                      </div>
+                      <div className = "col2">
+                        <i className='fas fa-map-marker'></i><p>500 m</p>
+                      </div>
+                    </div>
+                  </div>
+                    <button>Pick This Job!</button>
+              </div>
+            </div>
+            <div className = "card">
+              <div className = "lcolumn">
+                  <img src = "img/cleaning.jpg"></img>
+              </div>
+              <div className = "rcolumn">
+                <h1>Cleaning House</h1>
+                  <div className = "jobdetails">
+                    <div className = "rowdetails">
+                      <div className="col1">
+                        <i className='far fa-calendar-alt'></i><p>Today 5pm</p>
+                      </div>
+                      <div className="col2">
+                        <i className='fas fa-clock'></i><p>2 Hours</p>
+                      </div>
+                    </div>
+                    <div className = "rowdetails">
+                      <div className = "col1">
+                        <i className='fa fa-rupee'></i><p>100</p>
+                      </div>
+                      <div className = "col2">
+                        <i className='fas fa-map-marker'></i><p>200 m</p>
+                      </div>
+                    </div>
+                  </div>
+                    <button>Pick This Job!</button>
+              </div>
+            </div>
+            <div className = "card">
+              <div className = "lcolumn">
+                  <img src = "img/shop.jpg"></img>
+              </div>
+              <div className = "rcolumn">
+                <h1>Takecare of Shop</h1>
+                  <div className = "jobdetails">
+                    <div className = "rowdetails">
+                      <div className="col1">
+                        <i className='far fa-calendar-alt'></i><p>40 minutes</p>
+                      </div>
+                      <div className="col2">
+                        <i className='fas fa-clock'></i><p>3 hours</p>
+                      </div>
+                    </div>
+                    <div className = "rowdetails">
+                      <div className = "col1">
+                        <i className='fa fa-rupee'></i><p>300</p>
+                      </div>
+                      <div className = "col2">
+                        <i className='fas fa-map-marker'></i><p>5 km</p>
+                      </div>
+                    </div>
+                  </div>
+                    <button>Pick This Job!</button>
+              </div>
+            </div>
           </div>
         </div>
       )}
