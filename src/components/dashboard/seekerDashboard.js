8151680dbfe62dc7ed1449d9cd87cb254d765418
Sujal Modanwal <68843492+sujal-ux@users.noreply.github.com>
2025-04-10 23:44:34 +0530

    Reincarnation (#1)

--- src/components/dashboard/seekerDashboard.js
+++ src/components/dashboard/seekerDashboard.js
@@ -108,7 +108,7 @@ const JobPopup = (props)=>{
       onClose={props.handleClose}>
       <div className="flex relative">
         <div className="job-left-pane">
-          <div className="w-full text-gray-100 text-2xl my-8 px-6">Uber Hands</div>
+          <div className="w-full text-gray-100 text-2xl my-8 px-6">Dolet</div>
           <div className="wave-container">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
               <path fillOpacity="0.6" d="M0,96L34.3,112C68.6,128,137,160,206,186.7C274.3,213,343,235,411,224C480,213,549,171,617,133.3C685.7,96,754,64,823,85.3C891.4,107,960,181,1029,192C1097.1,203,1166,149,1234,128C1302.9,107,1371,117,1406,122.7L1440,128L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
